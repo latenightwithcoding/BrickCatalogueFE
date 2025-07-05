e588f980ac492ea010a585669c1da741d0ebfda6
minkuan-buu <minhquandoanngoc@gmail.com>
2025-07-05 23:25:50 +0700

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <div ref={scrollRef}>
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <section className="flex flex-col h-screen items-center justify-center gap-4 py-8 md:py-10">
           <div className="inline-block max-w-lg text-center justify-center">
             <span className={title()}>Make&nbsp;</span>
             <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
